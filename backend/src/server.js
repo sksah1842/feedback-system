a4b3ce2ad8fd3d02e5812d10fcb539a84533fc48
sksah <sksah1842@gmail.com>
2025-06-19 13:48:17 +0530

    some changes

--- backend/src/server.js
+++ backend/src/server.js
@@ -15,7 +15,11 @@ const io = initializeSocket(server);
 const allowedOrigin = process.env.FRONTEND_URL || 'http://localhost:5173';
 // app.use(cors({ origin: allowedOrigin, credentials: true }));
 app.use(cors({
-  origin: '*'
+  origin: [
+    "hhttps://feedback-system-hb6m-git-main-sumit-kumars-projects-b887fe80.vercel.app",
+    "http://localhost:5173"
+  ],
+  credentials: true
 }));
 app.use(express.json());
 app.use('/api/auth', authRoutes);
