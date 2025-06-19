8019ac2c398f71d5b52f90f6e16c971394f060e9
sksah <sksah1842@gmail.com>
2025-06-19 14:19:01 +0530

    some changes

--- backend/src/server.js
+++ backend/src/server.js
@@ -27,10 +27,7 @@ app.get('/', (req, res) => {
 app.use(express.json());
 app.use('/api/auth', authRoutes);
 app.use('/api/feedback', fbRoutes);
-app.get('/api/feedback', (req, res) => {
-  console.log("GET /api/feedback hit");
-  res.send([]); // or your actual logic
-});
+
 
 const PORT = process.env.PORT || 5000;
 mongoose.connect(process.env.MONGO_URI).then(() => {
