e6e8d3cadeab566be37d81f27cb9d285f17d625f
sksah <sksah1842@gmail.com>
2025-06-19 14:34:28 +0530

    some changes

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import axios from 'axios';
+import axios from '../lib/axios';
 
 export default function Login() {
   const [username, setUsername] = useState('');
